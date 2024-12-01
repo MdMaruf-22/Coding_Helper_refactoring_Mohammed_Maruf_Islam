aa50d822bf8958b58e01700b07b93e096c2ec87e
Maruf <marufislam74208@gmail.com>
2024-12-01 12:03:15 +0600

    Smell:blank method,so much commemts, also usage of old method like replaceAll,Refactor: replaced 'replaceAll' with 'replace'

--- searching/MethodFind.java
+++ searching/MethodFind.java
@@ -7,28 +7,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MethodFind {
-
-    //  public static ArrayList<String> Methodbody = new ArrayList<>();
     public void getMethod(String filename, String fileContent, String path, String processfilePath) throws FileNotFoundException, IOException {
 
         Scanner scan = new Scanner(fileContent);
         int linenumber = 0;
-        String file = scan.useDelimiter("\\Z").next().trim();             //^\w+\W\w+\.(\w+){3}\s*\{?+[^;]$ dotall
+        String file = scan.useDelimiter("\\Z").next().trim();
         String pattern = "(public|void|protected|private|static|final|public static|private static|protected static|public final|private final|protective final)+\\s*(\\<.*\\>)*\\s*[a-zA-Z]*\\s*\\b([_$a-zA-Z1-9]+)\\b\\s*\\(.*\\)\\s*[^;].*?$";
-        //  String pattern ="^(public)\\s+[a-zA-Z]*\\s+(\\bmethodName\\b)\\s*\\(\\)[^;]*$";
         Matcher methodMatcher = Pattern.compile(pattern, Pattern.MULTILINE).matcher(file);
         while (methodMatcher.find()) {
-            //   Search.count++;
             String method = new GrepContent().findBetweenBraces(methodMatcher.start(), file);
-            String methodname = methodMatcher.group().replaceAll("\\{", "").replaceAll("[\r\n]+", " ").trim();
-            //   System.out.println("matcher=" + methodname);
+            String methodname = methodMatcher.group().replace("\\{", "").replaceAll("[\r\n]+", " ").trim();
             linenumber = new GrepContent().getLineNumber(methodname, path, linenumber);
             String processFilename = methodMatcher.group(3) + "-" + linenumber + "-" + filename;;
-
             if (linenumber != 0) {
                 new ProcessSearchFile().processMethod(processFilename, method, path, processfilePath);
-
-                //   System.out.println("group="+methodMatcher.group());
             }
         }
     }
@@ -39,20 +31,16 @@ public class MethodFind {
         Matcher classMatcher = classpattern.matcher(fileContent);
         while (classMatcher.find()) {
             String className = classMatcher.group(1);
-            //  System.out.println("DF="+className);
             String classContent = new GrepContent().findBetweenBraces(classMatcher.start(), fileContent);
             Pattern constructorFind = Pattern.compile("(\\b" + classMatcher.group(1) + "\\b)\\s*\\(.*\\)\\s*[^;].*$", Pattern.MULTILINE);
 
             Matcher consMatch = constructorFind.matcher(classContent);
             while (consMatch.find()) {
                 String cons = consMatch.group();
-                //  Search.count++;
-                //   System.out.println("constructor=" + cons);
                 String constructor = new GrepContent().findBetweenBraces(consMatch.start(), classContent);
-                String ConsName = consMatch.group().replaceAll("\\{", "").replaceAll("[\r\n]+", " ").trim();
+                String ConsName = consMatch.group().replace("\\{", "").replaceAll("[\r\n]+", " ").trim();
                 linenumber = new GrepContent().getLineNumber(ConsName, path, linenumber);
                 String processFilename = consMatch.group(1) + "-" + linenumber + "-" + filename;
-                //    System.out.println("pro==" + processFilename);
                 if (linenumber != 0) {
                     new ProcessSearchFile().processMethod(processFilename, constructor, path, processfilePath);
                 }
