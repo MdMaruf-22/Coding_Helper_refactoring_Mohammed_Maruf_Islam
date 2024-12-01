0b1def61cfcc25bf17c2ed81080cec5fb1ec9509
Maruf <marufislam74208@gmail.com>
2024-12-01 11:32:27 +0600

    Smell:unused varaible,code commenting,and no check of dividaiton by zero refactor:removed those unused variables and comments,add a condition to check if vount is greater than 0)

--- code_clone/getTfIdf.java
+++ code_clone/getTfIdf.java
@@ -1,7 +1,6 @@
 package code_clone;
 import java.util.List;
 public class getTfIdf {
-
     public double getTf(String[] fileContent, String term) {
         double fileLength = fileContent.length;
         int count = 0;
@@ -10,27 +9,22 @@ public class getTfIdf {
                 count++;
             }
         }
-      //  System.out.println("tf="+count/fileLength);
         return count / fileLength;
     }
-
     public double getIdf(List allFile, String term) {
         double count = 0;
         double idf;
-  
         for (int i = 0; i < allFile.size(); i++) {
 
             String[] fileContent;
             fileContent = allFile.get(i).toString().split(" ");
             for (String ss : fileContent) {
-            //   System.out.println(""+ss);
                 if (ss.equalsIgnoreCase(term)) {
                     count++;
                     break;
-
                 }
             }
         }
-        return 1 + Math.log(allFile.size() / count);
+        if(count>0) return 1 + Math.log(allFile.size() / count);
     }
 }
