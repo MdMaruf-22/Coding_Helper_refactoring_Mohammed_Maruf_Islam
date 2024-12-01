d1bb11946879791922d6194c92c95b669dcee23d
Maruf <marufislam74208@gmail.com>
2024-12-01 11:59:16 +0600

    Smell:blank method, refactor:write something there

--- metrices/MethodCount.java
+++ metrices/MethodCount.java
@@ -3,9 +3,7 @@ package metrices;
 import IO.ProjectReader;
 import java.io.IOException;
 import searching.Search;
-
 public class MethodCount {
-
     public void getTotalMethods(String projectPath, String ProjectName) throws IOException {
         try{
         if (ProjectName.endsWith(".java")) {
@@ -18,8 +16,7 @@ public class MethodCount {
         ProjectReader.count = 0;
         Search.ProjectFileName.clear();
     }catch(Exception e){
-    
-    
+            e.print()
     }}
 }
 
