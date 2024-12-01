ed43056b9732e553a0ef83d1ebc399660ad7e431
Maruf <marufislam74208@gmail.com>
2024-12-01 12:10:59 +0600

    Smell:unuesd variable, refactor:remove thoseae

--- searching/Similarity.java
+++ searching/Similarity.java
@@ -1,5 +1,4 @@
 package searching;
-
 import code_clone.CosineSimilarity;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -8,35 +7,24 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import static searching.TfIdfCalculate.queryTfIdfVector;
-
 public class Similarity {
-
     public static HashMap<String, Double> SortDescending = new HashMap<>();
-
     public void getCosine() {
 
         double similarity = 0;
         for (int i = 0; i < Search.ProjectFileName.size(); i++) {
             int count = 0;
             double[] similar;
-            similar = new double[TfIdfCalculate.queryTfIdfVector.size()];
-
             for (int j = 0; j < queryTfIdfVector.size(); j++) {
                 similarity = new CosineSimilarity().cosineSimilarity(TfIdfCalculate.tfidfvectorProject.get(i), TfIdfCalculate.queryTfIdfVector.get(j));
-               BigDecimal bd = new BigDecimal(similarity).setScale(2, RoundingMode.HALF_UP);
+               BigDecimal bd = new BigDecimal.valueOf(similarity).setScale(2, RoundingMode.HALF_UP);
                 double getSimilar = bd.doubleValue();
-                
-//  System.out.println(Search.ProjectFileName.get(i) + "=" + similarity);
                 if (getSimilar > 0) {
                     SortDescending.put(Search.ProjectFileName.get(i), getSimilar);
-
                 }
-
             }
-
         }
     }
-
     public void getResult() {
         if(SortDescending.isEmpty()){
             System.out.println("\tSearch query not found");
@@ -44,11 +32,8 @@ public class Similarity {
         LinkedHashMap<String, Double> reverseSorted = new LinkedHashMap<>();
         SortDescending.entrySet()
                 .stream()
-                .sorted(HashMap.Entry.comparingByValue(Comparator.reverseOrder()))
+                .sorted(Entry.comparingByValue(Comparator.reverseOrder()))
                 .forEachOrdered(x -> reverseSorted.put(x.getKey(), x.getValue()));
-
-        //System.out.println("");
-        // System.out.println("");
         for (Map.Entry<String, Double> pair : reverseSorted.entrySet()) {
 
             System.out.println(pair.getKey() + " " + pair.getValue()+"%");
